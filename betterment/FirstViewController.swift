98d5127202714321fa76d2d3ae17f2e620ae23f2
Mitul Manish <mitul.manish@gmail.com>
2016-04-06 23:05:14 +1000

    Some UI love

--- betterment/FirstViewController.swift
+++ betterment/FirstViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import RealmSwift
 
 class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
@@ -15,15 +16,15 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
     var current_user: Person?
     
     var users: [Person] =
-        [Person(first_name: "Mitul", last_name: "Manish", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "I know my ABCD", skill: [Skill(skill_name: "Photography", hourly_rate: 56.87, experience: 5), Skill(skill_name: "Dancing", hourly_rate: 56.87, experience: 3),Skill(skill_name: "Sports Guidance", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-1.png", address: ""),
-    Person(first_name: "Ankur", last_name: "Dabral", email: "ankur.dabral@gmail.com", city: "Melbourne", bio: "I know my ABCD", skill: [Skill(skill_name: "Start Up Counselling", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-2.png", address: ""),
-    Person(first_name: "Praveen", last_name: "Mohan", email: "praveen.mohan@gmail.com", city: "Melbourne", bio: "I know my ABCD.I am the best", skill: [Skill(skill_name: "Video Editing", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-3.png", address: ""),
-    Person(first_name: "Shal", last_name: "Sajwan", email: "shal.sajwan@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Dancing", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-5.png", address: ""),
-    Person(first_name: "Bijin", last_name: "Abraham", email: "bijin.abraham@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Career Advice", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-5.png", address: ""),
-    Person(first_name: "John", last_name: "Abraham", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Fitness Training", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-5.png", address: ""),
-    Person(first_name: "Rohan", last_name: "Singh", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Sports Guidance", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-6.png", address: ""),
-    Person(first_name: "Jamie", last_name: "Shaw", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Python Programming", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-1.png", address: ""),
-    Person(first_name: "Ram", last_name: "Rahim", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Philosophy", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "people-2.png", address: "")]
+        [Person(first_name: "Mitul", last_name: "Manish", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "I know my ABCD", skill: [Skill(skill_name: "Photography", hourly_rate: 56.87, experience: 5), Skill(skill_name: "Dancing", hourly_rate: 56.87, experience: 3),Skill(skill_name: "Sports Guidance", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "man-a.png", address: ""),
+    Person(first_name: "Ankur", last_name: "Dabral", email: "ankur.dabral@gmail.com", city: "Melbourne", bio: "I know my ABCD", skill: [Skill(skill_name: "Start Up Counselling", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "man-b.png", address: ""),
+    Person(first_name: "Praveen", last_name: "Mohan", email: "praveen.mohan@gmail.com", city: "Melbourne", bio: "I know my ABCD.I am the best", skill: [Skill(skill_name: "Video Editing", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "man-c.png", address: ""),
+    Person(first_name: "Shal", last_name: "Sajwan", email: "shal.sajwan@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Dancing", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "man-d.png", address: ""),
+    Person(first_name: "Bijin", last_name: "Abraham", email: "bijin.abraham@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Career Advice", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "man-e.png", address: ""),
+    Person(first_name: "John", last_name: "Abraham", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Fitness Training", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "man-f.png", address: ""),
+    Person(first_name: "Rohan", last_name: "Singh", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Sports Guidance", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "man-g.png", address: ""),
+    Person(first_name: "Jamie", last_name: "Shaw", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Python Programming", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "woman-b.png", address: ""),
+    Person(first_name: "Ram", last_name: "Rahim", email: "mitul.manish@gmail.com", city: "Melbourne", bio: "anything", skill: [Skill(skill_name: "Philosophy", hourly_rate: 56.87, experience: 5)], work: [Work(title: "some random title", description: "desc", related_skill: "Photography", images: ["cutmypic.png"])], profile_image: "woman-a.png", address: "")]
     
     
     // self.performSegueWithIdentifier("showShop", sender: self)
@@ -41,6 +42,7 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
         print ("-------current user-----------")
         print(CURRENT_USER.authData.uid)
         print ("------------------")
+         print(Realm.Configuration.defaultConfiguration.path!)
         // find current user and save it in a variable
        
 
