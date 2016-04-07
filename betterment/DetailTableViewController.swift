94f12e3c6eec35ed78406315a28dbf216892847f
Mitul Manish <mitul.manish@gmail.com>
2016-04-08 01:03:33 +1000

    UI changes

--- betterment/DetailTableViewController.swift
+++ betterment/DetailTableViewController.swift
@@ -31,7 +31,7 @@ class DetailTableViewController: UIViewController, UITableViewDataSource, UITabl
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        //   = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
